2282667254d8d7741af2700325068affd410a4d7
Bertille Menguy <bertille.menguy@etudiant.univ-brest.fr>
2023-10-19 10:00:05 +0200

    nouveau

--- Gymnaste.gd
+++ Gymnaste.gd
@@ -1,17 +1,27 @@
 extends RigidBody3D
 
+@export var speed = 14
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 75
+
+var target_velocity = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Gestion de direction 
-	if Input.is_action_pressed("ui_up"):
-		apply_torque_impulse(Vector3(0,0,0))
+	# Do something...
+	pass
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("ui_right"):
-		apply_torque_impulse(Vector3(0,1,0))
+		apply_central_impulse(Vector3(1,0,0))
 	if Input.is_action_pressed("ui_left"):
-		apply_torque_impulse(Vector3(0,-1,0))
+		apply_central_impulse(Vector3(-1,0,0))
+	if Input.is_action_pressed("ui_down"):
+		apply_central_impulse(Vector3(0,0,1))
+	if Input.is_action_pressed("ui_up"):
+		apply_central_impulse(Vector3(0,0,-1))
